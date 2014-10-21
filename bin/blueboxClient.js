cb2def4ddf3ba675f816391599e1eb67caa87e08
Jesús Pérez <jesusprubio@gmail.com>
2014-10-21 19:19:13 +0200

    Catching all exceptions

--- bin/blueboxClient.js
+++ bin/blueboxClient.js
@@ -230,7 +230,7 @@ printer.info('If you have doubts just use the default options :)\n');
 createPrompt();
 
 // Just in case ;)
-//process.on('uncaughtException', function (err) {
-//    printer.error('"uncaughtException" found:');
-//    printer.error(err);
-//});
+process.on('uncaughtException', function (err) {
+    printer.error('"uncaughtException" found:');
+    printer.error(err);
+});
