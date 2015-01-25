de1bdf063f90001f504ef15aa2042356a3010c74
Sergio García Ramos <s3rgio.gr@gmail.com>
2015-01-25 20:51:00 +0100

    Whois module changed.

--- modules/whois.js
+++ modules/whois.js
@@ -1,6 +1,6 @@
 /*
 Copyright Jesus Perez <jesusprubio gmail com>
-
+          Sergio Garcia <s3rgio.gr gmail com>          
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
