9ad20a8dccb06a910c9d1644d84f551c2978c18d
Sergio García <s3rgio.gr@gmail.com>
2015-05-21 13:15:59 +0200

    amiVoiceMailUsersList module modified

--- modules/amiVoiceMailUsersList.js
+++ modules/amiVoiceMailUsersList.js
@@ -1,108 +1,106 @@
-///*
-//Copyright Jesus Perez <jesusprubio gmail com>
-//
-//This program is free software: you can redistribute it and/or modify
-//it under the terms of the GNU General Public License as published by
-//the Free Software Foundation, either version 3 of the License, or
-//(at your option) any later version.
-//
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
-//
-//You should have received a copy of the GNU General Public License
-//along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//*/
-//
-//'use strict';
-//
-//var async   = require('async'),
-//    namiLib = require('nami'),
-//    Nami    = namiLib.Nami,
-//
-//    printer = require('../utils/printer');
-//
-//
-//module.exports = (function () {
-//
-//    return {
-//
-//        info : {
-//            name        : 'amiVoiceMailUsersList',
-//            description : 'Use the Asterisk Manager service (AMI) to locate voicemail users',
-//            options     : {
-//                target : {
-//                    description  : 'IP address to brute-force',
-//                    defaultValue : '127.0.0.1',
-//                    type         : 'targetIp'
-//                },
-//                port : {
-//                    description  : 'Port of the server',
-//                    defaultValue : 5038,
-//                    type         : 'port'
-//                },
-//                user : {
-//                    description  : 'User to use in the request',
-//                    defaultValue : 'admin',
-//                    type         : 'anyValue'
-//                },
-//                password : {
-//                    description  : 'Password to use in the request',
-//                    defaultValue : 'amp111',
-//                    type         : 'anyValue'
-//                },
-//                timeout : {
-//                    description  : 'Time to wait for a response (ms.)',
-//                    defaultValue : 5000,
-//                    type         : 'positiveInt'
-//                }
-//            }
-//        },
-//
-//        run : function (options, callback) {
-//            var connected = false,
-//                data      = [],
-//                ami       = new Nami({
-//                    host     : options.target,
-//                    port     : options.port,
-//                    username : options.user,
-//                    secret   : options.password
-//                });
-//
-//            ami.logger.setLevel('OFF');
-//
-//            ami.on('namiConnected', function () {
-//                var action = new namiLib.Actions.VoicemailUsersList();
-//
-//                connected = true;
-//                ami.send(action, function (res) {
-//                    ami.close();
-//                    callback(null, data);
-//                });
-//            });
-//
-//            ami.on('namiRawMessage', function (evt) {
-//                data.push(evt);
-//            });
-//
-//            ami.on('namiLoginIncorrect', function () {
-//                callback({
-//                    type : 'login'
-//                });
-//            });
-//
-//            // The module do not supports connection timeout, so
-//            // we add it manually ("connected" var), really dirty trick
-//            setTimeout(function () {
-//                if (!connected) {
-//                    callback({
-//                        type : 'timeout'
-//                    });
-//                }
-//            }, options.timeout);
-//            ami.open();
-//        }
-//    };
-//
-//}());
+/*
+Copyright Jesus Perez <jesusprubio gmail com>
+          Sergio García <s3rgio.gr gmail com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+'use strict';
+
+// Private stuff
+
+var async   = require('async'),
+    namiLib = require('nami'),
+    Nami    = namiLib.Nami,
+
+    printer = require('../utils/printer'),  
+
+    HELP = {
+        description : 'Use the Asterisk Manager service (AMI) to locate voicemail users',
+        options     : {
+            target : {
+                type : 'ip',
+                description  : 'IP address to brute-force',
+                defaultValue : '127.0.0.1'
+            },
+            port : {
+                type         : 'port',
+                description  : 'Port of the server',
+                defaultValue : 5038
+            },
+            user : {
+                type         : 'allValid',
+                description  : 'User to use in the request',
+                defaultValue : 'admin'
+            },
+            password : {
+                type         : 'allValid',
+                description  : 'Password to use in the request',
+                defaultValue : 'amp111'
+            },
+            timeout : {
+                type         : 'positiveInt',
+                description  : 'Time to wait for a response (ms.)',
+                defaultValue : 5000
+            }
+        }
+    };
+
+// Public stuff
+
+module.exports.help = HELP;
+
+module.exports.run = function (options, callback) {
+   var connected = false,
+       data      = [],
+       ami       = new Nami({
+           host     : options.target,
+           port     : options.port,
+           username : options.user,
+           secret   : options.password
+       });
+
+   ami.logger.setLevel('OFF');
+
+   ami.on('namiConnected', function () {
+       var action = new namiLib.Actions.VoicemailUsersList();
+
+       connected = true;
+       ami.send(action, function (res) {
+           ami.close();
+           callback(null, data);
+       });
+   });
+
+   ami.on('namiRawMessage', function (evt) {
+       data.push(evt);
+   });
+
+   ami.on('namiLoginIncorrect', function () {
+       callback({
+           type : 'login'
+       });
+   });
+
+   // The module does not support connection timeout, so
+   // we add it manually ("connected" var), really dirty trick
+   setTimeout(function () {
+       if (!connected) {
+           callback({
+               type : 'timeout'
+           });
+       }
+   }, options.timeout);
+   ami.open();
+};
