9392eef0c49d5ede949dedd8507301e353e1f4d5
ancahy <dimelatuya@hotmail.com>
2016-08-09 00:23:40 +0200

    Update client.js

--- bin/client.js
+++ bin/client.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // Copyright Jesus Perez <jesusprubio gmail com>
-//
+//           Antonio Carrasco <ancahy2600 gmail com>
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
