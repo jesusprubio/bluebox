6dd034bf2f92400766569a51b4b6c1dde5dc61c6
Jesús Pérez <jesusprubio@gmail.com>
2016-08-19 00:40:13 +0200

    Rest of the commands of the client refactored and added.

--- bin/client.js
+++ bin/client.js
@@ -27,6 +27,12 @@ const utils = require('../lib/utils/utils');
 
 const debug = utils.debug(utils.pathToName(__filename));
 
+// Global paramameters to avoid the user having to rewrite them
+// in each module run. They are proposed as the default value.
+// They name should be equal to the one to module expected option
+// which should rewrite.
+const globalParams = {};
+
 
 debug('Starting ...');
 const bluebox = new Bluebox({});
@@ -36,6 +42,11 @@ debug('Modules details', modulesInfo);
 
 utils.debug('Defining the commands for the Bluebox modules ...');
 utils.each(utils.keys(modulesInfo), moduleName => {
+  // We need this to do a trick to include the global parameters.
+  // The Bluebox library manages the module parameters default values
+  // for us. But we need this trick to allow the global parameters.
+  const defaults = {};
+
   vorpal
     .command(moduleName)
     .description(modulesInfo[moduleName].description)
@@ -47,10 +58,18 @@ utils.each(utils.keys(modulesInfo), moduleName => {
         // Massaging the data to make Vorpal happy.
         utils.each(utils.keys(expectedOpts), name => {
           let message = `* ${name}: ${expectedOpts[name].description}`;
+          let finalDefault = null;
 
-          if (expectedOpts[name].defaultValue) {
-            message += ` (${expectedOpts[name].defaultValue})`;
+          if (globalParams[name]) {
+            finalDefault = globalParams[name];
+          } else if (expectedOpts[name].defaultValue) {
+            finalDefault = expectedOpts[name].defaultValue;
           }
+          if (finalDefault) {
+            defaults[name] = finalDefault;
+            message += ` (${finalDefault})`;
+          }
+          message += ': ';
 
           parsedOpts.push({
             type: 'input',
@@ -63,7 +82,17 @@ utils.each(utils.keys(modulesInfo), moduleName => {
         // with ES6: https://github.com/dthree/vorpal/issues/14
         vorpal.activeCommand.prompt(parsedOpts)
         .then(answers => {
-          bluebox.run(moduleName, answers)
+          const finalAnswers = [];
+
+          utils.each(utils.keys(answers), key => {
+            if (answers[key] === '') {
+              finalAnswers[key] = defaults[key];
+            } else {
+              finalAnswers[key] = answers[key];
+            }
+          });
+
+          bluebox.run(moduleName, finalAnswers)
           .then(res => {
             logger.bold('\nRESULT:\n');
             if (!res) {
@@ -76,7 +105,7 @@ utils.each(utils.keys(modulesInfo), moduleName => {
           })
           .catch(err => {
             // We always resolve (instead reject) because we don't
-            // want to stop the full program.
+            // want to break the full app.
             logger.error(`Running the module : ${err.message}`);
             resolve();
           });
@@ -85,13 +114,107 @@ utils.each(utils.keys(modulesInfo), moduleName => {
           logger.error(`Getting the options : ${err.message}`);
           resolve();
         });
-      })
-    );
+      }));
 });
+
+// Client specific commands.
+vorpal
+  .command('shodanSetKey')
+  .description('To add you SHODAN API key')
+  .action(() =>
+    new Promise(resolve => {
+      vorpal.activeCommand.prompt([
+        {
+          type: 'input',
+          name: 'key',
+          message: 'Your key: ',
+        },
+      ])
+      .then(answers => {
+        if (answers.key) {
+          bluebox.setShodanKey(answers.key);
+        } else {
+          logger.error('Empty key');
+        }
+        resolve();
+      })
+      .catch(err => {
+        logger.error(`Getting the key : ${err.message}`);
+        resolve();
+      });
+    }));
+
+
+vorpal
+  .command('globalGet')
+  .description('To get the value of a global parameter')
+  .action(() =>
+    new Promise(resolve => {
+      vorpal.activeCommand.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Name of the param: ',
+        },
+      ])
+      .then(answers => {
+        if (answers.name) {
+          if (globalParams[answers.name]) {
+            logger.result(globalParams[answers.name]);
+          } else {
+            logger.error('Global parameter not found');
+          }
+        } else {
+          logger.error('Empty name');
+        }
+
+        resolve();
+      })
+      .catch(err => {
+        logger.error(`Getting the key : ${err.message}`);
+        resolve();
+      });
+    }));
+
+
+vorpal
+  .command('globalSet')
+  .description('To add a global parameter to use through all the modules')
+  .action(() =>
+    new Promise(resolve => {
+      vorpal.activeCommand.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Name of the param: ',
+        },
+        {
+          type: 'input',
+          name: 'value',
+          message: 'New value to set: ',
+        },
+      ])
+      .then(answers => {
+        if (!answers.name) {
+          logger.error('Empty name');
+        } else if (!answers.value) {
+          logger.error('Empty value');
+        } else {
+          globalParams[answers.name] = answers.value;
+          logger.json(answers);
+        }
+
+        resolve();
+      })
+      .catch(err => {
+        logger.error(`Getting the key : ${err.message}`);
+        resolve();
+      });
+    }));
+
 
 logger.welcome('\n\tWelcome to Bluebox-ng');
 logger.info(`\t(v${bluebox.version()})\n`);
-
 
 debug('Starting the prompt ...');
 vorpal
