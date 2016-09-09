151238674958abdc8f6a2748fcc83c1e7384d801
Jesús Pérez <jesusprubio@gmail.com>
2016-09-09 13:04:18 +0200

    Using the proper comments in each place.

--- artifacts/installScripts/kali2.sh
+++ artifacts/installScripts/kali2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Copyright 	Jesus Perez <jesusprubio gmail com>
-#		Aan Wahyu <cacaddv gmail com>	
+# Copyright   Jesus Perez <jesusprubio gmail com>
+#             Aan Wahyu <cacaddv gmail com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
