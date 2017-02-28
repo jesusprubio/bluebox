263cb61208034a87e87b6f64dfbf85ab3a291421
Jesús Pérez <jesusprubio@gmail.com>
2017-02-28 19:56:33 +0100

    Minor fixes in Kali installation script.

--- artifacts/installScripts/kali2.sh
+++ artifacts/installScripts/kali2.sh
@@ -3,18 +3,8 @@
 # Copyright   Jesus Perez <jesusprubio gmail com>
 #             Aan Wahyu <cacaddv gmail com>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# This code may only be used under the GPLv3 license found at
+# http://www.gnu.org/licenses/gpl-3.0.txt.
 
 
 # Script to install Bluebox-ng in Kali 2 GNU/Linux
@@ -25,13 +15,13 @@ echo "Checking aptitude..."
 
 # Check for aptitude is installed or not. Because default kali linux does not provide aptitude
 if dpkg-query -s "aptitude" 1>/dev/null 2>&1; then
-    echo "OK. Aptitude is installed..."
-  else
-    if apt-cache show aptitude 1>/dev/null 2>&1; then
-	echo "Installing aptitude..."
-	apt-get -y install aptitude
-     fi
+  echo "OK. Aptitude is installed ..."
+else
+  if apt-cache show aptitude 1>/dev/null 2>&1; then
+	  echo "Installing aptitude ..."
+	  apt-get -y install aptitude
   fi
+fi
 
 # Just in case the first boot (or Live cd)
 echo "Updating sources ... (just in case)"
@@ -42,6 +32,5 @@ curl -sL https://deb.nodesource.com/setup_6.x | bash -
 aptitude install -y nodejs
 
 echo "Installing Bluebox-ng, wait a moment please ..."
-# still commented because bluebox-ng in npm repo not updated. just comment this if already launched
-#npm i -g bluebox-ng
+npm i -g bluebox-ng
 echo "Done, just type 'bluebox-ng' :)"
