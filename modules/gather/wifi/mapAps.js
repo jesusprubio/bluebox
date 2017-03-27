9def20748fe652a61b04bd1719b2557a4ed9cfb7
Jesús Pérez <jesusprubio@gmail.com>
2017-03-27 21:58:38 +0200

    Wifi protocol created.

---
+++ modules/gather/wifi/mapAps.js
@@ -0,0 +1,31 @@
+/*
+  Copyright Jesús Pérez <jesusprubio@gmail.com>
+
+  This code may only be used under the MIT license found at
+  https://opensource.org/licenses/MIT.
+*/
+
+'use strict';
+
+const scanner = require('node-wifiscanner');
+
+// const utils = require('../../../lib/utils');
+
+
+module.exports.desc = 'Wifi access point mapper.';
+
+
+// TODO: Not working.
+// module.exports.impl = () => utils.promisify(scanner.scan);
+
+module.exports.impl = () =>
+  new Promise((resolve, reject) => {
+    scanner.scan((err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve(data);
+    });
+  });
