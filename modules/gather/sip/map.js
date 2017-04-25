aac1ac11cb6f023bbfc1c35b6707474f13125112
Jesús Pérez <jesusprubio@gmail.com>
2017-04-25 18:24:22 +0200

    Minor fix with the default concurrency.

--- modules/gather/sip/map.js
+++ modules/gather/sip/map.js
@@ -8,7 +8,7 @@
 'use strict';
 
 const mapper = require('../../../lib/mapper');
-const map = require('../../../lib/protocols/sip').map;
+const proto = require('../../../lib/protocols/sip');
 const optsMap = require('../../../cfg/commonOpts/map');
 const optsSip = require('../../../cfg/commonOpts/sip');
 const utils = require('../../../lib/utils');
@@ -19,8 +19,8 @@ utils.defaultsDeep(optsComm, optsMap, optsSip);
 optsComm.rports.default = [5060, 5061];
 // It's better to scan with an OPTIONS request (commonly less restricted)
 optsComm.meth.default = 'OPTIONS';
-// TODO: Confirm a good limit here.
-// optsComm.concurrency.default = proto.concurrency;
+// We hace control over this library so we can increase it with security.
+optsComm.concurrency.default = proto.concurrency;
 
 module.exports.desc = 'SIP service mapper.';
 
@@ -28,4 +28,4 @@ module.exports.desc = 'SIP service mapper.';
 module.exports.opts = optsComm;
 
 
-module.exports.impl = (opts = {}) => mapper(opts.rhosts, map, opts);
+module.exports.impl = (opts = {}) => mapper(opts.rhosts, proto.map, opts);
