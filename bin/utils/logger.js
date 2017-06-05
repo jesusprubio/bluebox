33355032da29555b7acabe1553909996f1dc6c88
Jesús Pérez <jesusprubio@gmail.com>
2017-06-05 22:15:39 +0200

    More methods added to the logger.
    ie: To print large arrays in chunks.

--- bin/utils/logger.js
+++ bin/utils/logger.js
@@ -10,10 +10,15 @@
 const clc = require('cli-color');
 const prettyjson = require('prettyjson');
 const emoji = require('node-emoji');
+const keypress = require('keypress');
+
+const utils = require('../../lib/utils');
 
 
 /* eslint-disable no-console */
 module.exports.regular = str => console.log(str);
+
+module.exports.bold = str => console.log(clc.bold(str));
 
 module.exports.info = str => console.log(clc.xterm(55)(str));
 
@@ -28,16 +33,61 @@ module.exports.error = (str, err) => {
   if (err && err.stack) { console.log(clc.red(err.stack)); }
 };
 
-// module.exports.clear = () => process.stdout.write(clc.reset());
-
 module.exports.title = str => console.log(clc.bold.xterm(202)(str));
-
-module.exports.subtitle = str => console.log(clc.bold(str));
 
 module.exports.time = label => console.time(clc.xterm(63)(label));
 
 module.exports.timeEnd = label => console.timeEnd(clc.xterm(63)(label));
+
+module.exports.chunks = (arr, chunkSize, done) => {
+  if (!utils.isArray(arr)) { throw new Error('Bad format, array needed'); }
+
+  const chunks = utils.chunk(arr, chunkSize);
+  let index = 0;
+  const eventName = 'keypress';
+
+  // Make `process.stdin` begin emitting "keypress" events.
+  keypress(process.stdin);
+
+
+  console.log(prettyjson.render(chunks[index]));
+  index += 1;
+  if (!chunks[index]) {
+    done();
+    return;
+  }
+
+  console.log(clc.xterm(63)('\nPress ESC to finish or any other key to see more ...'));
+  process.stdin.on(eventName, (ch, key) => {
+    // To allow ctrl+c.
+    if (key.ctrl && key.name === 'c') {
+      process.stdin.pause();
+    }
+
+    if ((key.name && (key.name === 'c' || key.name === 'escape')) ||
+        !chunks[index]) {
+      process.stdin.removeAllListeners(eventName);
+      done();
+      return;
+    }
+
+    console.log(prettyjson.render(chunks[index]));
+    index += 1;
+  });
+
+  if (process.stdin) {
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+  }
+};
+
 /* eslint-enable no-console */
 
 module.exports.emoji = label => emoji.get(label);
 
+module.exports.moveUp = () => {
+  process.stdout.write(clc.move.up(1));
+  process.stdout.write(clc.erase.line);
+};
+
+module.exports.clear = () => process.stdout.write(clc.reset);
